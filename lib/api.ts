8e236d5d7c4ece01c79ed883e9e96b9496f3427e
staq001 <dame.hush@gmail.com>
2026-01-05 18:10:09 +0100

    update api base url

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://expense-tracker-cohu.onrender.com";
+const API_BASE_URL = process.env.API_URL as string;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
